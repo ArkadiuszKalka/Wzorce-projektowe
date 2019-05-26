cf55829f331965247f9ebe1445958d8f92915ba2
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-05-26 15:35:33 +0200

    Wzorce projektowe Singleton

---
+++ src/main/java/eu/infoshare/pl/Logger.java
@@ -0,0 +1,28 @@
+package eu.infoshare.pl;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Logger {
+
+    private static Logger LOG_INSTANCE;
+
+
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    private Logger() {
+    }
+
+    private static Logger getInstance() {
+        synchronized (Logger.class) {
+            if (LOG_INSTANCE == null) {
+                LOG_INSTANCE = new Logger();
+            }
+            return LOG_INSTANCE;
+        }
+    }
+
+    private AtomicInteger increment() {
+        counter.incrementAndGet();
+        return counter;
+    }
+}
